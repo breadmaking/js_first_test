8c0f9b501fa51996cb9d9564154e4ec77653b460
breadmaking <liquidgoldlabel@gmail.com>
2022-02-28 16:16:34 +0000

    Testing Push

---
+++ add.test.js
@@ -0,0 +1,14 @@
+const add = require('./add');
+
+describe('add', () => {
+  // test cases
+  it('adds 2 and 2', () => {
+    //TODO write test
+    expect(add(2,2)).toBe(4)
+  });
+
+  it('adds 0 and 5', () => {
+    expect(add(0,5)).toBe(5)
+  });
+
+});
