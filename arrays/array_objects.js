2c55eb113743abba1740a50170eaa269e5b21e19
breadmaking <liquidgoldlabel@gmail.com>
2022-02-28 18:19:02 +0000

    chapter 17 done

--- arrays/array_objects.js
+++ arrays/array_objects.js
@@ -15,3 +15,17 @@ const customers = [
 ];
 
 console.log(generateMessages(customers));
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const isEvenNumber = (number) => {
+  if (number % 2 == 0) {
+    return number;
+  } else {
+    //pass
+  }
+};
+
+const evenNumbers = numbers.map((number) => {
+  return isEvenNumber(number);
+});
